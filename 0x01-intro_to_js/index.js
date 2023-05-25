1e24307d65bbc55a278c31a10fcb5df10347c62a
donkor samuel asante <dsasante1@gmail.com>
2023-05-25 23:20:54 +0000

    done task 0x05

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,21 @@
+let myName = "samuel";
+
+console.log(typeof myName);
+
+myName = "asante"
+
+console.log(myName);
+
+let car = {
+    manufacturer: "Toyota",
+    color: "red",
+    model: "corrolla",
+    yearManufactured:"2022",
+    numberOfSeats: 4,
+    mileage: 200,
+    engineSize:"max",
+    fuelType: "eletric",
+    transmission: "manual",
+    horsepower: 500
+
+};
