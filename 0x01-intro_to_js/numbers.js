f65d55d063dcf4d3207a77b3a8845e4cea320de4
donkor samuel asante <dsasante1@gmail.com>
2023-05-25 23:34:36 +0000

    done task 0x06

--- 0x01-intro_to_js/numbers.js
+++ 0x01-intro_to_js/numbers.js
@@ -1,1 +1,13 @@
- 
+let favoriteNumber = 8;
+
+let halfOfFavNum = favoriteNumber / 2;
+
+let isaacsFavorite = 13;
+
+let subtract = isaacsFavorite - favoriteNumber;
+
+isaacsFavorite = isaacsFavorite * 26;
+
+isaacsFavorite %= 3;
+
+
