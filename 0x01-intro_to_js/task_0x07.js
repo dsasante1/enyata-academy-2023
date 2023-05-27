3adc848b726f3e78a485c3c214170bbd7e87f6e4
donkor samuel asante <dsasante1@gmail.com>
2023-05-27 16:45:25 +0000

    add conditionals

--- 0x01-intro_to_js/task_0x07.js
+++ 0x01-intro_to_js/task_0x07.js
@@ -1,55 +1,73 @@
 
+// user inputs
 
-//user inputs
+const name = prompt(' Welcome write a story about your favourite team!\nwhat is your name? : '); // string
+const gender = prompt('what is your gender? : '); // string
+const age = parseInt(prompt('how old are you? : ')); // number
+const storyName = prompt('Now, what is the title of your story? : '); // string
 
+const favouriteTeam = prompt('what is your favourite football team?:'); // string
+const rivalTeam = prompt('what is the name of their rivals'); // string
+const goalsScored = parseInt(prompt('How many goals did your favourite team score against their rivals the last time they playede?: ',0)); // number
+const distanceCovered = parseFloat(prompt("What is the average  distance covered by your team's players in the match? : ", 30.0)); // float
+let isRefreeBiased = prompt('Was the refree biased? true or false: ', true); // bool
+let isGoalKeeperGood = prompt('your teams goalkeeper, is he good? true or false : ', true); // bool
 
-let name = prompt("what is your name?: "); //string
-let gender = prompt("what is your gender?: "); //string
-let age = prompt("how old are you? "); //number
-let story_name = prompt("Now, what is the title of your story? "); //string
-
-let players_team; //string
-let opposing_team; //string
-let final_scores; //number
-let distance; // float
-let foul; //bool
-let keeper_saves; //bool
-
-
-//prompt("hello world");
+// prompt("hello world");
 
 // story template
-function fullStory(name, gender, age, story_name){
-    var story_template = `My name is ${name}. I am ${gender}. I am ${age} years old.
-The title of my story is ${story_name}...`;
-  return story_template;
+function fullStory(name, gender, age, storyName, favouriteTeam, rivalTeam, goalsScored, distanceCovered, isRefreeBiased, isGoalKeeperGood){
+
+  let pastVictories;
+  let gameIntensity;
+  let biasedRefree;
+  let goodGoalKeeper;
+
+  let storyTemplate = `My name is ${name}. I am ${gender}. I am ${age} years old.
+The title of my story is ${storyName}...
+
+My favourite team is ${favouriteTeam} their sworn rival is ${rivalTeam}.
+In fact if ${favouriteTeam} is given a gun and loaded with two bullets and instructed to shoot the devil and ${rivalTeam}.
+${favouriteTeam} will shoot ${rivalTeam} twice in the head.`
+
+  if (goalsScored > 3){
+      pastVictories = `The last time ${favouriteTeam} met ${rivalTeam}, ${favouriteTeam} scored  ${goalsScored} goals against ${rivalTeam}. It was a mighty defeat.`;
+      }else{
+      pastVictories = `The last time ${favouriteTeam} met ${rivalTeam}, ${favouriteTeam} scored  ${goalsScored} goals against ${rivalTeam}.`;
+      }
+
+  if (distanceCovered > 30){
+        gameIntensity = `${favouriteTeam} dominated the game covering a staggering ${distanceCovered}km on the football pitch.
+        ${rivalTeam} were clearly out of their depths, dazed, and moving haphazardly like a chicken without a head. Fans of ${favouriteTeam} roared with cheers and started celebrating before the final whistle was blown.
+        It was an incredible game.`;
+  }else{
+        gameIntensity = `Although it was a lacklustre performance by ${favouriteTeam}, ${rivalTeam} was worse.
+        ${rivalTeam}They were just not good enough. ${rivalTeam} relies on their past glories and hay days without nothing to show for it in the current football dispensation.`;
+      }
+
+  if (isRefreeBiased == true){
+      biasedRefree = `The refree had an agenda against ${favouriteTeam}. He was on the pitch with the sole intention of winnging the game for ${rivalTeam}.`;
+  }else{
+      biasedRefree = `Suprisingly the refree was not biased and his decisions were top notch. It was a fair game between ${favouriteTeam} and ${rivalTeam}`;
+  }
+
+  if (isGoalKeeperGood == true && isRefreeBiased == true){
+      goodGoalKeeper = `The ${favouriteTeam}'s goalkeeper was in his elements.
+      He saved the day.`;
+  }else if (isGoalKeeperGood == false && isRefreeBiased == true){
+      goodGoalKeeper = `${favouriteTeam} maintained their lead to the end of the match.`;
+  }
+  else if(isGoalKeeperGood == false && isRefreeBiased == false){
+      goodGoalKeeper = `${favouriteTeam} goalkeeper was not in his elements but the fortunately ${rivalTeam} could not utilize this opportunity.`;
+  }else{
+      goodGoalKeeper = `At the end of the game ${favouriteTeam} emerged victorious`;
+    }
+
+  return storyTemplate + pastVictories + gameIntensity + gameIntensity + biasedRefree + goodGoalKeeper;
 }
 
-let y = fullStory(name, gender, age, story_name);
+const output = fullStory(name, gender, age, storyName, favouriteTeam, rivalTeam,
+  goalsScored, distanceCovered, isRefreeBiased, isGoalKeeperGood);
 
-alert(y);
+alert(output);
 
-//console.log(story_template);
-
-//requirements
-
-
-/*
-1. Your story should be at least 50 words
-long and not more than 100 words.
-
-
-2.  You are to use a minimum of 10 variables
- of varying types.
-*/
-
-
-// TIPS
-
-/*
-[TIP 1]: {_variable_} means a variable should be there of name '_variable_'
-[TIP 2]: Use prompt to get the values of the variables from the user.
-[TIP 3]: Collect all variables at the beginning.
-[TIP 4]: Put the story in a variable within the function and it should be displayed when the function is run.
-
-*/
